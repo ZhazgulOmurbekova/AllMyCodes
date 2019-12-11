1e177b44fa979908975c2f9bafe12748941ad425
ZhazgulOmurbekova <kjazggul@gmail.com>
2019-12-10 19:54:52 -0600

    all codeaa

---
+++ src/enumEx/BaceEnum.java
@@ -0,0 +1,10 @@
+package enumEx;
+
+public class BaceEnum {
+    enum LetterGrade {A,B,C}
+
+    public static void main(String[] args) {
+        LetterGrade grade;
+        grade = LetterGrade.A;
+    }
+}
---
+++ src/enumEx/DBUtility.java
@@ -0,0 +1,18 @@
+package enumEx;
+
+public class DBUtility {
+    Connection connection;
+    public void openConnection(DataBase db){
+        switch (db){
+            case USERS:
+                //connection = conect
+                // code to connect to user db
+            case MEDIA:
+                //connection = connect
+                // code to connect to sale db
+            case VIDEO:;
+
+
+        }
+    }
+}
